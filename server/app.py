c284f96a81f898d95fadc5d2b586eac70d3dbf24
hirotoshi12 <hirotoshi.666k66@gmail.com>
2022-06-20 23:44:33 +0900

    'gitに追加（仮）'

--- server/app.py
+++ server/app.py
@@ -1,28 +1,29 @@
 from flask import Flask
 
+# 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
 
 # タスクの一覧の取得(GET)...データの取得
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-  return {}
+  return {'GET':'データの取得'}
 # 'GET':'データの取得'
 
 # タスクの追加(POST)...データの新規作成
 @app.route('/tasks', methods=['POST'])
 def post_tasks():
-  return {}
+  return {'POST':'データの新規作成'}
 # 'POST':'データの新規作成'
 
 # タスクの完了(PUT)...データの更新
 @app.route('/tasks', methods=['PUT'])
 def put_tasks():
-  return {}
+  return {'PUT':'データの更新'}
 # 'PUT':'データの更新'
 
 # タスクの削除(DELETE)...データの削除
 @app.route('/tasks/<task_id>', methods=['DELETE'])
 def delete_tasks():
-  return {}
+  return {'DELETE':'データの削除'}
 # 'DELETE':'データの削除'
