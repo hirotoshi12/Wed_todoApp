39edd7e4272ada0472759c3b0ed99dda2729b72c
hirotoshi12 <hirotoshi.666k66@gmail.com>
2022-07-22 21:46:05 +0900

    'メソッドの修正'

--- server/app.py
+++ server/app.py
@@ -1,29 +1,51 @@
-from flask import Flask
+from flask import Flask, request
 
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
+# 文字化け対策
+app.config['JSON_AS_ASCII'] = False
+
+todo_id = 0
+
+todo = {
+    'new': {},
+    'end': {},
+}
 
 # タスクの一覧の取得(GET)...データの取得
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-  return {'GET':'データの取得'}
-# 'GET':'データの取得'
+    status = request.args.get("status")
+    if status == "new":
+        return todo['new']
+    elif status == "end":
+        return todo['end']
+    else:
+        return {}
 
 # タスクの追加(POST)...データの新規作成
 @app.route('/tasks', methods=['POST'])
 def post_tasks():
-  return {'POST':'データの新規作成'}
-# 'POST':'データの新規作成'
+    global todo_id
+    global todo
+    todo_id += 1
+    todo['new'][todo_id]  = request.get_json()['task']
+    return {
+        'id': todo_id,
+        'content': todo['new'][todo_id]
+    }
 
 # タスクの完了(PUT)...データの更新
-@app.route('/tasks', methods=['PUT'])
-def put_tasks():
-  return {'PUT':'データの更新'}
-# 'PUT':'データの更新'
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def put_tasks(task_id):
+    todo['end'][task_id] = todo['new'].pop(task_id)
+    return {task_id: todo['end'][task_id]}
 
 # タスクの削除(DELETE)...データの削除
-@app.route('/tasks/<task_id>', methods=['DELETE'])
-def delete_tasks():
-  return {'DELETE':'データの削除'}
-# 'DELETE':'データの削除'
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_tasks(task_id):
+  # 1. pop関数を使って取得と削除を行う。
+  remove_task = todo['new'].pop(task_id)
+  # 2. return でpop関数から返ってきた値を返す
+  return {task_id: remove_task}
